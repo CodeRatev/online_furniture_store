0af6764509b9f29c6565d218100ecf81700028a6
CodeRatev <shotokan2003@gmail.com>
2024-05-04 23:51:31 +0300

    Fixed some mistakes.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Prefetch
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 from django.contrib import auth, messages
 from carts.models import Cart
@@ -74,9 +76,17 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
     
+    orders = Order.objects.filter(user=request.user).prefetch_related(
+                Prefetch(
+                    "orderitem_set",
+                    queryset=OrderItem.objects.select_related("product"),
+                )
+            ).order_by("-id")
+    
     context = {
         'title': '12 стульев - Кабинет',
-        'form': form
+        'form': form,
+        'orders': orders
     }
     return render(request, 'users/profile.html', context)
 
